755daade0b5fd30010547c6cbf7ef8da1dd9a5af
PrateekJaiswal12 <140104968+PrateekJaiswal12@users.noreply.github.com>
2025-03-27 15:17:17 +0530

    Delete customer_service directory

--- customer_service/utility_services/forms.py
+++
@@ -1,9 +0,0 @@
-# utility_services/forms.py
-
-from django import forms
-from .models import ServiceRequest
-
-class ServiceRequestForm(forms.ModelForm):
-    class Meta:
-        model = ServiceRequest
-        fields = ['type_of_request', 'details', 'attached_file']  # Add other fields as needed
